2ac3e058d3678f140805edd4e5068c4ba68fddd8
Jakob Lybarger <jakoblybarger911@gmail.com>
2023-11-06 22:41:25 -0600

    Remove unused sync.Map in main.go

--- main.go
+++ main.go
@@ -4,18 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 	"os"
-)
 
-import (
 	handlers "go-url-shortener/handlers"
 )
-
-// Global map to store the URL and its corresponding short URL
-// For now, this is just a simple Map and is not persisted anywhere.
-// todo later: Persist the map in a proper database with a caching layer.
-var urlMap sync.Map
 
 func main() {
 	http.HandleFunc("/shorten", handlers.ShortenHandler)
