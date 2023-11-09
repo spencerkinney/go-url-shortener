229ccb8b4ba90e1c890af36960081fe0e504e148
spencerkinney <spencer_kinney@yahoo.com>
2023-11-08 22:16:03 -0600

    slight CORS change to handle the OPTIONS request

--- url-shortener/main.go
+++ url-shortener/main.go
@@ -9,32 +9,17 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-// func main() {
-// 	http.HandleFunc("/shorten", CORS(handlers.ShortenHandler))
-// 	http.HandleFunc("/", CORS(handlers.HomeOrRedirectHandler))
-
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = "8080"
-// 	}
-
-// 	fmt.Printf("Server listening on port %s\n", port)
-// 	log.Fatal(http.ListenAndServe(":"+port, nil))
-// }
-
 func main() {
 	requestHandler := func(ctx *fasthttp.RequestCtx) {
-		// fmt.Println("Received inbound request")
-		var (
-			allowOrigin      = "*"
-			allowCredentials = "true"
-			allowHeaders     = "Authorization accept Content-Type"
-			allowedMethods   = "GET, POST"
-		)
-		ctx.Response.Header.Set("Access-Control-Allow-Origin", allowOrigin)
-		ctx.Response.Header.Set("Access-Control-Allow-Credentials", allowCredentials)
-		ctx.Response.Header.Set("Access-Control-Allow-Headers", allowHeaders)
-		ctx.Response.Header.Set("Access-Control-Allow-Methods", allowedMethods)
+		// Set CORS headers
+		setCORSHeaders(ctx)
+
+		// Handle preflight requests
+		if string(ctx.Method()) == "OPTIONS" {
+			// Preflight request only needs headers
+			ctx.SetStatusCode(fasthttp.StatusOK)
+			return
+		}
 
 		path := string(ctx.Path())
 		switch path {
@@ -55,26 +40,11 @@ func main() {
 	if err := fasthttp.ListenAndServe(":"+port, requestHandler); err != nil {
 		log.Fatalf("Error in ListenAndServe: %v", err)
 	}
-	// else {
-	// 	fmt.Printf("Server listening on port %s\n", port)
-	// }
 }
 
-// func CORS(next fasthttp.RequestHandler) fasthttp.RequestHandler {
-// 	// fmt.Println("Setting CORS for context")
-// 	var (
-// 		allowOrigin      = "*"
-// 		allowCredentials = "true"
-// 		allowHeaders     = "Access-Control-Allow-Headers, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"
-// 		allowedMethods   = "GET, POST"
-// 	)
-// 	return func(ctx *fasthttp.RequestCtx) {
-// 		// fmt.Println("Setting response headers for context")
-// 		ctx.Response.Header.Set("Access-Control-Allow-Origin", allowOrigin)
-// 		ctx.Response.Header.Set("Access-Control-Allow-Credentials", allowCredentials)
-// 		ctx.Response.Header.Set("Access-Control-Allow-Headers", allowHeaders)
-// 		ctx.Response.Header.Set("Access-Control-Allow-Methods", allowedMethods)
-
-// 		next(ctx)
-// 	}
-// }
+func setCORSHeaders(ctx *fasthttp.RequestCtx) {
+	ctx.Response.Header.Set("Access-Control-Allow-Origin", "*")
+	ctx.Response.Header.Set("Access-Control-Allow-Credentials", "true")
+	ctx.Response.Header.Set("Access-Control-Allow-Headers", "Authorization, Accept, Content-Type")
+	ctx.Response.Header.Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+}
